62374ec2142f21a582d2e4aa6e475ed08dd10e63
AtulChourasia <atul2002chourasia@gmail.com>
2023-06-11 18:29:08 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,102 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { hoverChangeExperience } from "./hoverChangeExperience.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeExperience(
+  ".digitalhouse",
+  `Worked as a frontend Intern, Helped Renewing UI of the official
+  website. Designed Frontend of the website(Figma).Debuged and
+  optimized their official website(React.js). Interacted with
+  users. Mainly worked with (React, javascript, HTML, CSS, Figma)`,
+  "Front-End Developer(Intern)",
+  "Webiknowr",
+  "Feb 2020 - Apr 2020 (2 months)"
+);
+
+hoverChangeExperience(
+  ".zuplae",
+  `Developed a Basic UI for Banking managment system. and completed all the tasks assigned by seniors.worked mainly on (React.js, HTML, JS, CSS)`,
+  "Web Developer (Intern)",
+  "The Sparks Foundation",
+  "Jan 2021 - Feb 2021 (1 Month)"
+);
+
+hoverChangeExperience(
+  ".codigofontetv",
+  `As, I was preparing for competetive exam I did'nt join any Company during my 3rd and 4th year of graduation. But I kept on pollishing my skills and Making new Achivements <br> - I Cleared GATE(2022) & GATE(2023) - CSE and Achived (AIR-4500) <br>
+  - I redesign existing UIs to better adapt Android and IOS devices (Cloned Entire Nike E-com Website)
+   <br>
+  - I like Presenting UI concepts in my projects and getting
+  feedbacks from profesionals.<br>
+  - Built strong Programing Concepts and enginnering fundamentsls(DSA,OS,COA,DBMS,C-programing) during GATE prepration`,
+  "Personal Project and Achivements",
+  "",
+  "Feb 2021 - Present"
+);
+
+// hoverChangeExperience(
+//   ".contweb",
+//   `As, I was preparing for competetive exam I did'nt join any Company during my 3rd and 4th yrar of graduation. But I kept on pollishing my skills and Making new Achivements <br> - I Cleared GATE(2022) & GATE(2023) - CSE and Achived (AIR-4500) <br>
+//   - I redesign existing UIs to better adapt Android and IOS devices (Cloned Entire Nike E-com Website)
+//    <br>
+//   - I like Presenting UI concepts in my projects and getting
+//   feedbacks from profesionals.`,
+//   "Developer frontend e UI Designer",
+//   "Personal Projects and Achivements",
+//   "Feb 2021 - Present"
+// );
+
+hoverChangeDescription(
+  ".html",
+  "HTML is a markup language, where we mark elements to define what information the page will display.<br>"
+);
+hoverChangeDescription(
+  ".css",
+  "CSS is a style sheet language composed of “layers”, created for the purpose of styling pages."
+);
+hoverChangeDescription(
+  ".js",
+  "JavaScript is a programming language that allows you to implement dynamic elements on web pages."
+);
+hoverChangeDescription(
+  ".sass",
+  "Sass is a CSS preprocessor that adds some features that aren't available natively."
+);
+hoverChangeDescription(
+  ".react",
+  "React is a JavaScript library focused on building user interfaces in a componentized way."
+);
+hoverChangeDescription(
+  ".next",
+  "Next.js is a web framework that enables functionality like server-side rendering and static website generation for React-based web sites."
+);
+hoverChangeDescription(
+  ".styled",
+  "styled-components is a library that uses the concept of CSS-in-JS, that is, it allows us to write CSS codes inside Javascript."
+);
+hoverChangeDescription(
+  ".tailwind",
+  "Tailwind CSS is a CSS framework that provides us with utility classes for the purpose of styling pages."
+);
+hoverChangeDescription(
+  ".typescript",
+  "TypeScript is a superset of JavaScript that includes features that are not natively present in the language, in addition to making it static."
+);
+// hoverChangeDescription(
+//   ".radix",
+//   "Radix é uma biblioteca que fornece componentes acessíveis e sem estilos para a criação de aplicação em React."
+// );
+// hoverChangeDescription(
+//   ".cypress",
+//   "Cypress é um framework para automação de testes end-to-end, onde atualmente usa a linguagem JavaScript."
+// );
+// hoverChangeDescription(
+//   ".storybook",
+//   "Storybook é uma ferramenta que tem como objetivo documentar e testar os componentes da aplicação."
+// );
