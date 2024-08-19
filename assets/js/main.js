22f2aa161a7929e411a58e3a9e219b91f40016fc
Atul Chourasia <67797718+AtulChourasia@users.noreply.github.com>
2024-08-19 22:39:11 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,15 +29,14 @@ hoverChangeExperience(
 
 hoverChangeExperience(
   ".codigofontetv",
-  `As, I was preparing for competetive exam I did'nt join any Company during my 3rd and 4th year of graduation. But I kept on pollishing my skills and Making new Achivements <br> - I Cleared GATE(2022) & GATE(2023) - CSE and Achived (AIR-4500) <br>
-  - I redesign existing UIs to better adapt Android and IOS devices (Cloned Entire Nike E-com Website)
-   <br>
-  - I like Presenting UI concepts in my projects and getting
-  feedbacks from profesionals.<br>
-  - Built strong Programing Concepts and enginnering fundamentsls(DSA,OS,COA,DBMS,C-programing) during GATE prepration`,
-  "Personal Project and Achivements",
+  ` <br>
+  - Qualified GATE 2022 and GATE 2023 with above 96 percentile.<br>
+  - Solved over 500 coding problems across various platforms, showcasing strong problem-solving skills. <br>
+  - Leetcode Top 19% in contest rating.<br>
+  - As, I was preparing for competetive exam I did'nt join any Company during my 3rd and 4th year of graduation. But I kept on pollishing my skills and Making new Achivements<br>`,
+  "Achivements",
   "",
-  "Feb 2021 - Present"
+  "Present"
 );
 
 // hoverChangeExperience(
